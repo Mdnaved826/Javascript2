501f0c3adca71c9c60d9f43fa9f0a032072136e1
Mdnaved826 <naved.github12@gmail.com>
2026-02-21 10:55:40 +0530

    J.S DOM-1
    This is DOM Part-1

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("hello!");
+alert("apna-college");
+window.console.log("hello");
+console.dir(document.body);
+console.log(document.body);
+document.body.childNodes[3].innerText = "abcd";
